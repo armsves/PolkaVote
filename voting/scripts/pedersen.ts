2eceba715327f99bd6d73b2fbdd9e8f91f109c7d
crypdoughdoteth <zkorn1@icloud.com>
2025-07-17 21:50:54 +0200

    WIP: Script for pedersonCommit

---
+++ voting/scripts/pedersen.ts
@@ -0,0 +1,17 @@
+import { Barretenberg, Fr } from "@aztec/bb.js";
+import { Noir } from "@noir-lang/noir_js";
+
+const BLINDING_FACTOR = 420;
+
+async function gmPc(): Promise<void> {
+    const bb: Barretenberg = await Barretenberg.new();
+    const buffer = new ArrayBuffer(8);
+    const buf = new DataView(buffer, 0);
+    buf.setInt8(1, 42);
+    buf.getInt8(1); // 42
+    var uint8array = new Uint8Array(buf.buffer);
+    bb.pedersenCommit([Fr.fromBuffer(uint8array)], BLINDING_FACTOR);
+}
+
+console.log(await gmPc());
+
