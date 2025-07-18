87ef130f71bb677951c26ed2a51949a401b4cf29
crypdoughdoteth <zkorn1@icloud.com>
2025-07-18 02:13:24 +0200

    Bug: pedersen commit hangs

--- voting/scripts/pedersen.ts
+++ voting/scripts/pedersen.ts
@@ -4,12 +4,17 @@ const BLINDING_FACTOR = 420;
 
 async function gmPc(): Promise<void> {
     const bb: Barretenberg = await Barretenberg.new();
-    const buffer = new ArrayBuffer(8);
-    const buf = new DataView(buffer, 0);
-    buf.setInt8(1, 42);
-    var uint8array = new Uint8Array(buf.buffer);
-    bb.pedersenCommit([Fr.fromBuffer(uint8array)], BLINDING_FACTOR);
+    // const buffer = new ArrayBuffer(8);
+    // const buf = new DataView(buffer, 0);
+    // buf.setInt8(1, 42);
+    // var uint8array = new Uint8Array(buf.buffer);
+    const random = Fr.random();
+    console.log(random);
+    await bb.pedersenCommit([random], BLINDING_FACTOR);
 }
 
-console.log(await gmPc());
+gmPc().catch((e) => {
+    console.log(e)
+    process.exit(1);
+});
 
