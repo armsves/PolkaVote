61b4e3e622800a086fd2b2917e44416c021b57a6
gil7788 <gil7788@gmail.com>
2025-08-08 11:29:04 +0300

    Clean Project and Add Open Vote Circuits

---
+++ verifier_contracts/script/DeployVotingVerifier.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+// import {HonkVerifier} from "../src/Verifier.sol";
+// import {InscriptionVerifier} from "../src/InscriptionVerifier.sol";
+import {HonkVerifier} from "../src/VotingVerifier.sol";
+
+contract DeployVotingVerifier is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        // Deploy the shared HonkVerifier
+        // HonkVerifier verifier = new HonkVerifier();
+
+        // Deploy InscriptionVerifier with HonkVerifier
+        // InscriptionVerifier inscriptionVerifier = new InscriptionVerifier(verifier);
+
+        // Deploy VotingVerifier with HonkVerifier
+        HonkVerifier verifier = new HonkVerifier();
+
+        vm.stopBroadcast();
+    }
+}
